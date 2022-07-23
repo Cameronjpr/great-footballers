1aa20bf56d44ad69fd2193802c9a78531036714f
cameronjpr <cameronjpr@gmail.com>
2022-07-23 21:05:13 +0100

    initial commit

---
+++ src/data/players.ts
@@ -0,0 +1,39 @@
+type Player = {
+	name: string;
+	position: string;
+};
+
+export const players: Player[] = [
+	{
+		name: 'Zinedine Zidane',
+		position: 'Midfielder'
+	},
+	{
+		name: 'Lionel Messi',
+		position: 'Midfielder'
+	},
+	{
+		name: 'Johan Cruyff',
+		position: 'Midfielder'
+	},
+	{
+		name: 'Diego Maradonna',
+		position: 'Midfielder'
+	},
+	{
+		name: 'Ferenc Puskás',
+		position: 'Midfielder'
+	},
+	{
+		name: 'Stanley Matthews',
+		position: 'Midfielder'
+	},
+	{
+		name: 'Ronaldo Nazario',
+		position: 'Midfielder'
+	},
+	{
+		name: 'Andrés Iniesta',
+		position: 'Midfielder'
+	}
+];
---
+++ src/routes/index.svelte
@@ -0,0 +1,81 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import { players } from '../data/players';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>Football’s greatest</h1>
+
+	<section class="player-list">
+		{#each players as player}
+			<article class="player-card">
+				<img src="https://source.unsplash.com/random/?footballer" alt="A footballer" />
+				<h2>{player.name}</h2>
+				<span>{player.position}</span>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+					ut labore et dolore magna aliqua.
+				</p>
+			</article>
+		{/each}
+	</section>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+		color: #c57b57;
+		font-family: var(--font-hero);
+	}
+
+	h2 {
+		color: #f7dba7;
+		font-size: 30px;
+		/* font-family: var(--font-hero); */
+	}
+
+	.player-list {
+		display: flex;
+		gap: 1rem;
+		flex-flow: column nowrap;
+	}
+
+	.player-card {
+		width: 30rem;
+		padding: 1rem;
+		border-radius: 1rem;
+		background-color: var(--primary-color);
+		color: white;
+		transition: all 0.5s ease-in-out none;
+	}
+	.player-card:hover {
+		outline: auto;
+		transform: scale(1.02);
+	}
+
+	.player-card p {
+		color: inherit;
+	}
+
+	.player-card img {
+		width: 100%;
+		height: 10rem;
+		object-fit: cover;
+	}
+</style>
